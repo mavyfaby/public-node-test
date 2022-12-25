bdcdb19fd6ea37b5a6c335363cfdd2c440c1f673
Maverick G. Fabroa <maverickfabroa@gmail.com>
2022-12-25 19:53:16 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.type("application/json").status(200).send("Node.js + Express Server Test by Mavyy");
+});
+
+app.listen(PORT, () => {
+  console.log("Listening on port " + PORT);
+});
