c2441be62f924485693fd431f545030542052160
Maverick G. Fabroa <maverickfabroa@gmail.com>
2022-12-25 21:10:30 +0800

    fix: add process.env.PORT

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.type("application/json").status(200).send("Node.js + Express Server Test by Mavyy");
