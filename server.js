f0ab523874bb75236d64b871e83c38df05c5e1ad
mavyfaby <maverickfabroa@gmail.com>
2022-12-29 08:58:22 +0800

    chore(name): change message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.type("application/json").status(200).send("Node.js + Express Server Test by Mavyy");
+  res.type("application/json").status(200).send("Node.js + Express Server Test by Mavyfaby");
 });
 
 app.listen(PORT, () => {
